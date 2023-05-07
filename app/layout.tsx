3f2dd4a1b6b880cc07c6bc1ca878596033d52625
murillowelsi <murillo.welsi@gmail.com>
2023-05-07 09:10:14 +0100

    created register modal and its components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import { PT_Sans_Caption } from 'next/font/google'
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
 import NavBar from './components/navbar/NavBar'
 import './globals.css'
+import ToasterProvider from './providers/ToasterProvider'
 
 const font = PT_Sans_Caption({ subsets: ['latin'], weight: '700' })
 
@@ -17,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font.className bg-zinc-800`}>
-        <NavBar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <NavBar />
+        </ClientOnly>
         {children}
       </body>
     </html>
