065ae0ff8c7eae4b51e79711f606b360f34b19fe
murillowelsi <murillo.welsi@gmail.com>
2023-05-13 08:40:03 +0100

    add individual card feature

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -9,8 +9,8 @@ interface HeadingProps {
 const Heading: React.FC<HeadingProps> = ({ title, subtitle, center }) => {
   return (
     <div className={center ? 'text-center' : 'text-start'}>
-      <div className="text-2-xl font-bold">{title}</div>
-      <div className="mt-2 font-light text-neutral-400">{subtitle}</div>
+      <div className="text-2-xl font-bold text-zinc-300">{title}</div>
+      <div className="mt-2 font-light text-neutral-300">{subtitle}</div>
     </div>
   )
 }
